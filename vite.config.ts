754b59610e94dcabf96ec4d2c599f491d6c399e2
sairaakram <sairaakram20001@gmail.com>
2026-06-07 12:19:53 +0500

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const backendTarget = process.env.VITE_API_BASE_URL || 'http://localhost:53691'
+const backendTarget = process.env.VITE_API_BASE_URL || 'http://sairaakram-001-site1.mtempurl.com'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
